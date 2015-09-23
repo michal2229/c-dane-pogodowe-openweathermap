e7a9c16da82e9b3c8382ae97ed43cdba8e37ce27
michal_229 <michal_229@o2.pl>
2015-09-23 02:30:04 +0200

    watek w tle, skomentowane debugowanie writeline, mozliwe, ze trzeba dorzucic kilka zabezpieczen przed wpisaniem zlych danych

--- pogoda/Program.cs
+++ pogoda/Program.cs
@@ -9,7 +9,9 @@ namespace pogoda {
 			Console.Write ("Prosze wpisac nazwe miasta (bez polskich znakow): ");
 			String city = Console.ReadLine();
 			Console.Write ("Prosze wpisac liczbe dni, ktore ma obejmowac prognoza <1..16>: ");
-			String days = Console.ReadLine();
+			int days = Int32.Parse(Console.ReadLine());
+			Console.Write ("Prosze wpisac okres sprawdzania pogody [min]: ");
+			int okresMin = Int32.Parse(Console.ReadLine());
 
 			int cityId = getCityID (city, "../../dane/city.list.json"); // lista pobrana z http://bulk.openweathermap.org/sample/city.list.json.gz
 
@@ -17,29 +19,36 @@ namespace pogoda {
 				Console.WriteLine ("Nie mozna znalezc miasta"); return;
 			}
 
-			Console.Write ("ID miasta to: ");
-			Console.WriteLine (cityId);
+			Console.WriteLine ("ID miasta to: " + cityId);
+			Console.WriteLine ("Aby zakonczyc nacisnij enter...");
 
-			String weatherDataJson = getForecast (cityId, days);
 
-			WeatherData danePogodowe = getWeatherDataFromJson (weatherDataJson);
-			Temp srednieTemperatury = averageTempDays (danePogodowe);
-			double sredniaRoznica = averageDelta (srednieTemperatury);
+			BackgroundTask bt1 = new BackgroundTask(cityId, days, okresMin);
+			System.Threading.Thread t1 = new System.Threading.Thread(new System.Threading.ThreadStart(bt1.keepChecking));
+			t1.Start();
+
+			//while (!t1.IsAlive);
+
+			Console.ReadLine();
+			t1.Abort (); t1.Join ();
+
+			Console.WriteLine ("Koniec programu.");
 		}
+			
 
 
 		/////////// metody pomocnicze
 
-		static double averageDelta(Temp tempAvg) {
-			Console.WriteLine ("metoda averageDelta()");
-			Console.WriteLine ("averageDelta: " + (tempAvg.max - tempAvg.min));
-			Console.WriteLine ("koniec metody averageDelta()");
+		public static double averageDelta(Temp tempAvg) {
+			//Console.WriteLine ("metoda averageDelta()");
+			//Console.WriteLine ("averageDelta: " + (tempAvg.max - tempAvg.min));
+			//Console.WriteLine ("koniec metody averageDelta()");
 			return tempAvg.max - tempAvg.min;
 		}
 
 
-		static Temp averageTempDays(WeatherData wd1) { // liczenie sredniej		
-			Console.WriteLine ("metoda averageTempDays()");
+		public static Temp averageTempDays(WeatherData wd1) { // liczenie sredniej		
+			//Console.WriteLine ("metoda averageTempDays()");
 
 			Temp temp1 = new Temp();
 
@@ -47,45 +56,45 @@ namespace pogoda {
 			foreach (Days d in wd1.list)
 				temp1.day += d.temp.day;
 			temp1.day /= wd1.list.Length;
-			Console.WriteLine ("avg day: " + temp1.day);
+			//Console.WriteLine ("avg day: " + temp1.day);
 
 			//min
 			foreach (Days d in wd1.list)
 				temp1.min += d.temp.min;
 			temp1.min /= wd1.list.Length;
-			Console.WriteLine ("avg min: " + temp1.min);
+			//Console.WriteLine ("avg min: " + temp1.min);
 
 			//max
 			foreach (Days d in wd1.list)
 				temp1.max += d.temp.max;
 			temp1.max /= wd1.list.Length;
-			Console.WriteLine ("avg max: " + temp1.max);
+			//Console.WriteLine ("avg max: " + temp1.max);
 
 			//night
 			foreach (Days d in wd1.list)
 				temp1.night += d.temp.night;
 			temp1.night /= wd1.list.Length;
-			Console.WriteLine ("avg night: " + temp1.night);
+			//Console.WriteLine ("avg night: " + temp1.night);
 
 			//eve
 			foreach (Days d in wd1.list)
 				temp1.eve += d.temp.eve;
 			temp1.eve /= wd1.list.Length;
-			Console.WriteLine ("avg eve: " + temp1.eve);
+			//Console.WriteLine ("avg eve: " + temp1.eve);
 
 			//morn
 			foreach (Days d in wd1.list)
 				temp1.morn += d.temp.morn;
 			temp1.morn /= wd1.list.Length;
-			Console.WriteLine ("avg morn: " + temp1.morn);
+			//Console.WriteLine ("avg morn: " + temp1.morn);
 
-			Console.WriteLine ("koniec metody averageTempDays()");
+			//Console.WriteLine ("koniec metody averageTempDays()");
 			return temp1;
 		}
 
 
-		static Temp convertKelvinToCelsius(Temp temp) {
-			Console.WriteLine ("koniec metody averageTempDays()");
+		public static Temp convertKelvinToCelsius(Temp temp) {
+			//Console.WriteLine ("metoda convertKelvinToCelsius()");
 			Temp temp1 = new Temp ();
 
 			temp1.day = temp.day - 273.15;
@@ -95,40 +104,40 @@ namespace pogoda {
 			temp1.eve = temp.eve - 273.15;
 			temp1.morn = temp.morn - 273.15;
 		
-			Console.WriteLine ("koniec metody averageTempDays()");
+			//Console.WriteLine ("koniec metody convertKelvinToCelsius()");
 			return temp1;
 		}
 
 
-		static WeatherData getWeatherDataFromJson(String weatherJson) {
-			Console.WriteLine ("metoda getWeatherDataFromJson()");
+		public static WeatherData getWeatherDataFromJson(String weatherJson) {
+			//Console.WriteLine ("metoda getWeatherDataFromJson()");
 
 			WeatherData wd1 = Newtonsoft.Json.JsonConvert.DeserializeObject<WeatherData> (weatherJson); // deserializacja linii json	
 
-			Console.WriteLine ("koniec metody getWeatherDataFromJson()");
+			//Console.WriteLine ("koniec metody getWeatherDataFromJson()");
 			return wd1;
 		}
 
 
-		static String getForecast(int cityId, String days) { // api.openweathermap.org/data/2.5/forecast/daily?id={city ID}&cnt={cnt}; id: city ID; cnt: number of days returned (from 1 to 16)
-			Console.WriteLine ("metoda getForecast()");
+		public static String getForecast(int cityId, int days) { // api.openweathermap.org/data/2.5/forecast/daily?id={city ID}&cnt={cnt}; id: city ID; cnt: number of days returned (from 1 to 16)
+			//Console.WriteLine ("metoda getForecast()");
 			string htmlCode;
 
 			using (System.Net.WebClient client = new System.Net.WebClient ()) 
 			{
 				client.QueryString.Add("id", cityId.ToString()); 
-				client.QueryString.Add("cnt", days); 
+				client.QueryString.Add("cnt", days.ToString()); 
 				htmlCode = client.DownloadString("http://api.openweathermap.org/data/2.5/forecast/daily");
 			}
 
-			Console.WriteLine (htmlCode);
-			Console.WriteLine ("koniec metody getForecast()");
+			//Console.WriteLine (htmlCode);
+			//Console.WriteLine ("koniec metody getForecast()");
 			return htmlCode;
 		}
 
 
-		static int getCityID(String cityName, String listPath) { 
-			Console.WriteLine ("metoda getCityID()");
+		public static int getCityID(String cityName, String listPath) { 
+			//Console.WriteLine ("metoda getCityID()");
 
 			int counter = 0;
 			string line; // znaleziona linia json zawierajaca szukane miasto
@@ -153,10 +162,61 @@ namespace pogoda {
 			}
 
 
-			Console.WriteLine ("koniec metody getCityID()");
+			//Console.WriteLine ("koniec metody getCityID()");
 			return c1.id;
 		}
 	}
+		
+
+
+	/////////// klasa w tle
+
+	public class BackgroundTask
+	{
+		int cityId;
+		int days;
+		int okresMinut;
+
+		public BackgroundTask(int cityId, int days, int okresMinut) {
+			this.cityId = cityId;
+			this.days = days;
+			this.okresMinut = okresMinut;
+		}
+
+
+		public void keepChecking()
+		{
+			while (true)
+			{
+				String weatherDataJson = MainClass.getForecast (cityId, days);
+				String dataCzas = DateTime.Now.ToString (new System.Globalization.CultureInfo("en-GB")).Replace("/", "-").Replace(" ", "_").Replace(":", "-");
+				System.IO.File.WriteAllText(cityId + "_" + days + "_" + dataCzas + ".json", weatherDataJson);
+
+				string[] fileArray = Directory.GetFiles(Directory.GetCurrentDirectory(), cityId + "_" + days + "_*.json");
+
+				Console.Clear ();
+
+				foreach (string s in fileArray) {
+					string danePogodoweJson = File.ReadAllText(s);
+
+					WeatherData danePogodowe = MainClass.getWeatherDataFromJson (danePogodoweJson);
+					Temp srednieTemperatury = MainClass.averageTempDays (danePogodowe);
+
+					srednieTemperatury = MainClass.convertKelvinToCelsius (srednieTemperatury);
+
+					double sredniaRoznica = MainClass.averageDelta (srednieTemperatury);
+				
+					string dataPliku = (new System.Text.RegularExpressions.Regex (@"/.*/.+?_.+?_|\.json")).Replace(s, "");
+					Console.WriteLine (dataPliku);
+					Console.WriteLine ("srednie wartosci temperatur: " + srednieTemperatury.ToString());
+					Console.WriteLine ("srednia roznica min-max: " + sredniaRoznica);
+					Console.WriteLine ();
+				}
+
+				System.Threading.Thread.Sleep(okresMinut*1000*60);
+			}
+		}
+	};
 
 
 	/////////// klasy pomocnicze do parsowania json
@@ -183,6 +243,10 @@ namespace pogoda {
 		public double night { get; set; }
 		public double eve { get; set; }
 		public double morn { get; set; }
+
+		public override string ToString() {
+			return String.Format("rano: {0}, dzien: {1}, wieczor: {2}, noc: {3}, min: {4}, max: {5}", morn, day, eve, night, min, max);
+		}
 	}
 
 	public class Weather
